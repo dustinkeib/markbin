886f36a6ed99ec5a57a2235d52e1c8993b2bfabd
Dustin Keib <dustin@keib.net>
2017-03-19 13:47:03 -0700

    publish bins

--- server/main.js
+++ server/main.js
@@ -1,6 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import { Bins } from '../imports/collections/bins'
 
+// code to run on server at startup
 Meteor.startup(() => {
-  // code to run on server at startup
+  // Reg function so you can access this
+  Meteor.publish('bins', function() {
+    return Bins.find({ ownerId: this.userId })
+  })
 });
