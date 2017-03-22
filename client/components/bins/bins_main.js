36911e3bc219f3a0ec3f81c738d152a2882f1f11
Dustin Keib <dustin@keib.net>
2017-03-21 22:30:33 -0700

    wire up viewer

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { createContainer } from 'meteor/react-meteor-data'
 import { Bins } from '../../../imports/collections/bins'
 import BinsEditor from './bins_editor'
+import BinsViewer from './bins_viewer'
 
 class BinsMain extends Component {
   render() {
@@ -17,7 +18,10 @@ class BinsMain extends Component {
     // this.props.params.binId to get binId
     // console.log(this.props.bin)
     return (
-      <div><BinsEditor bin={this.props.bin} /></div>
+      <div>
+        <BinsEditor bin={this.props.bin} />
+        <BinsViewer bin={this.props.bin} />
+      </div>
     )
   }
 }
