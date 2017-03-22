daa57368b92ec204daa67baaba0e5a747160b013
Dustin Keib <dustin@keib.net>
2017-03-21 18:07:36 -0700

    dont forget

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 class BinsMain extends Component {
   render() {
+    // this.props.params.binId to get binId
     return (
       <div>BinsMain</div>
     )
